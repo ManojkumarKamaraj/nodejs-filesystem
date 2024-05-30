0cf5737efe4870e1048bc09dbbb1cd3fed04c7a2
ManojkumarKamaraj <manomk2020@gmail.com>
2024-05-30 20:12:27 +0530

    Completed Nodefilesystem

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# NodeJS-Day1 Task
+This contains Create and Retrive Data Endpoints Tasks in NodeJS
+
+Both Endpoints are created and i have added the Api Documentaion
+for preference
+
+https://documenter.getpostman.com/view/35315149/2sA3Qv7pzC
---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express"
+import fs from 'fs'
+import {format} from "date-fns";
+import path from "path";
+
+// importing phase
+
+// declaration phase
+const app = express();
+const PORT = 4000;
+
+// middleware
+
+app.use(express.json())
+
+const datetoday = format(new Date(),'dd-MM-yyyy-HH-mm-ss')
+const filepath = `Timestamp/${datetoday}.txt`
+const pathfolder = path.dirname(filepath); // TimeStamp
+
+const filename = path.basename(filepath); // file creation time and extension
+
+
+app.get('/create-date',(req,res)=>{
+    fs.writeFileSync(filepath,`${datetoday}`,"utf-8");
+    res.status(200).send(`The file created on the folder : ${pathfolder} and the file name is: ${filename}`);
+})
+
+app.get('/read-date',(req,res)=>{
+    const whole = path.resolve(`${pathfolder}`)
+
+    const data =fs.readdirSync(whole,"utf-8");
+    // console.log(data); to check all files are getting 
+    res.status(200).json({AllFiles: `${data}`});
+})
+
+// running port
+app.listen(PORT,()=>{
+    console.log(`app is running on the port ${PORT}`);
+})
